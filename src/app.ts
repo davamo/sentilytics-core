3d85897eda145591dc1dc6dc8701e747e4e402c8
developer <developer@developerM2.local>
2025-04-10 10:18:10 -0400

    feat: implement swagger

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,20 @@
 import express from 'express';
 import cors from 'cors';
 import sentimentRoutes from './routes/analyze.routes';
+import swaggerUi from 'swagger-ui-express';
+import swaggerJSDoc from 'swagger-jsdoc';
+
+const swaggerSpec = swaggerJSDoc({
+  definition: {
+    openapi: '3.0.0',
+    info: { title: 'Sentilytics API', version: '1.0.0' },
+  },
+  apis: ['./src/routes/*.ts'], // Ajusta según tu estructura
+});
 
 const app = express();
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.use(cors());
 app.use(express.json());
